cc235520d6c94398ac65891d1ac3178bb998adb6
Vladislav Korzun <Vladislav_K@abbyy.com>
2018-10-10 19:07:59 +0300

    Добавлена инициализация ролей

---
+++ PracticeApp/Models/RolesInitializer.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace PracticeApp.Models
+{
+    public static class RolesInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "admin@gmail.com";
+            string password = "_Aa123456";
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await roleManager.FindByNameAsync("user") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("user"));
+            }
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User { Email = adminEmail, UserName = adminEmail, Year = 2000 };
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+        }
+    }
+}
